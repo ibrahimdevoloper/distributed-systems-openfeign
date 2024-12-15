465928e833a2a3b0d21009b5381cfaf73c1fed2a
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-12-15 18:32:08 +0100

    added eureka

--- src/main/java/com/miskolc/openfeign/OpenfeignApplication.java
+++ src/main/java/com/miskolc/openfeign/OpenfeignApplication.java
@@ -1,13 +1,19 @@
 package com.miskolc.openfeign;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 @EnableFeignClients
 public class OpenfeignApplication {
 
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
 	public static void main(String[] args) {
 		SpringApplication.run(OpenfeignApplication.class, args);
 	}
